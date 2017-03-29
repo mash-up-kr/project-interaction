3e48dc93b65e9a558c4fab0301b1fa4732efdb01
taehyun.park <taehyun.park@navercorp.com>
2017-03-30 03:04:47 +0900

    initial commit

---
+++ project-interaction-BenPark/CircleActivityIndicator/Example/CircleActivityIndicator/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CircleActivityIndicator
+//
+//  Created by ppth0608 on 03/30/2017.
+//  Copyright (c) 2017 ppth0608. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
